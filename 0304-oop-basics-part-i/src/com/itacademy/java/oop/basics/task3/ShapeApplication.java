7513afc18354a2f71616cba5cbbba97c35c151b8
ziviledauderiene <zivile.dauderiene@gmail.com>
2022-03-08 12:10:38 +0200

    add 0304 ShapeApplication

---
+++ 0304-oop-basics-part-i/src/com/itacademy/java/oop/basics/task3/ShapeApplication.java
@@ -0,0 +1,22 @@
+package com.itacademy.java.oop.basics.task3;
+
+public class ShapeApplication {
+    public static void main(String[] args) {
+        Square rectangle1 = new Square(10, 20);
+        Square rectangle2 = new Square(-1, 2);
+        Square rectangle3 = new Square(5, 5);
+        Square rectangle4 = new Square(3, 15);
+
+        Square[] shapes = {rectangle1, rectangle2, rectangle3, rectangle4};
+
+        for (Square shape : shapes) {
+            if (shape.getLength() > 0 && shape.getWidth() > 0) {
+                shape.print(shape);
+            } else if (shape.getLength() < 0) {
+                System.out.println("l= " + shape.getLength() + ". Length can't have negative value.\n");
+            } else if (shape.getWidth() < 0) {
+                System.out.println("w= " + shape.getWidth() + ". Width can't have negative value.\n");
+            }
+        }
+    }
+}
---
+++ 0304-oop-basics-part-i/src/com/itacademy/java/oop/basics/task3/Square.java
@@ -0,0 +1,34 @@
+package com.itacademy.java.oop.basics.task3;
+
+public class Square {
+    private double length;
+    private double width;
+
+    public Square(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double calculatePerimeter() {
+        return (length + width) * 2;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public void print(Square shape) {
+        System.out.println("l: " + shape.getLength() + ", w: " + shape.getWidth());
+        System.out.println("Area: " + shape.calculateArea());
+        System.out.println("Perimeter: " + shape.calculatePerimeter());
+        System.out.println("");
+    }
+}
